1b799ce6fd9892f0ae885c77f24b45c4efbd4ebb
davidperezboza <david.perez@babelgroup.com>
2025-01-29 18:31:07 +0100

    Video 267

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,11 +1,15 @@
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
+import {pokemonApi} from '../../../api';
 
 export const getPokemons = (page = 0) => {
     return async (dispatch, getState) => {
         dispatch(startLoadingPokemons());
 
         //TODO: Realizaro petición http
-        //data-pokemons
-        //dispatch(setPokemons(data-pokemons));
+        //const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+        //const data = await resp.json();
+        const {data} = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
+
+        dispatch(setPokemons({pokemons: data.results, page: page + 1}));
     };
 };
